3717d8798896b92940287a3b8a42eb7922f739a4
jianglimin <jianglimin999@gmail.com>
2019-08-19 22:59:42 +0800

    login未完成

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react'
-import { Button } from 'antd'
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 export default class App extends Component {
   render(){
-    return  <Button type="primary">Button</Button>
+    return  (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}>
+
+          </Route>
+          <Route path='/' component={Admin}>
+
+          </Route>
+        </Switch>
+
+      </BrowserRouter>
+    )
   }
 }
